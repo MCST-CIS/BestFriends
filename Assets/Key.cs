e850f256c5f4a396ee4c580ab719b4a743b04a94
Ekansh Simgekar <echoekansh@gmail.com>
2026-05-09 19:32:09 -0400

    Added Finish Line and key for door

---
+++ Assets/Key.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject door;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("key picked up");
+            door.GetComponent<BoxCollider2D>().enabled = false;
+
+            this.gameObject.SetActive(false);
+        }
+    }
+}
